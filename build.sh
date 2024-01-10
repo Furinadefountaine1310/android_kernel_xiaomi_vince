d2293ef94b48597e7adb9bbd91029decb91aa161
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2024-01-10 14:45:41 +0100

    Linux 4.9.370
    From v4.14.336-EOL
    
    Link: https://lore.kernel.org/r/20240108141854.158274814@linuxfoundation.org
    Tested-by: Linux Kernel Functional Testing <lkft@linaro.org>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Pavel Machek (CIP) <pavel@denx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- build.sh
+++ build.sh
@@ -1,137 +1,78 @@
 #!/bin/bash
-#
-# Copyright (C) 2020 azrim.
-# All rights reserved.
 
-# Init
-KERNEL_DIR="${PWD}"
-KERN_IMG="${KERNEL_DIR}"/out/arch/arm64/boot/Image.gz-dtb
-ANYKERNEL=/workspace/Anykernel3
-COMPILER_STRING="Aosp Clang"
+echo -e "==========================="
+echo -e "= START COMPILING KERNEL  ="
+echo -e "==========================="
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-# Repo URL
-CLANG_REPO="https://gitlab.com/nekoprjkt/aosp-clang"
-ANYKERNEL_REPO="https://github.com/nekoprjkt/Anykernel3"
-ANYKERNEL_BRANCH="vince"
-
-# Compiler
-CLANG_DIR=/workspace/clang
-#if ! [ -d "${CLANG_DIR}" ]; then
-       git clone "$CLANG_REPO" --depth=1 "$CLANG_DIR"
-#fi
-
-# git clone https://github.com/baalajimaestro/aarch64-maestro-linux-android.git -b 07032020-9.2.1 --depth=1 "${KERNEL_DIR}/gcc"
-# git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi.git -b 07032020-9.2.1 --depth=1 "${KERNEL_DIR}/gcc32"
-
-# Defconfig7
-DEFCONFIG="vince-perf_defconfig"
-REGENERATE_DEFCONFIG="false" # unset if don't want to regenerate defconfig
-
-# Costumize
-KERNEL="NekoLite-Kernel"
-DEVICE="Vince"
-KERNELTYPE="EOL"
-KERNELNAME="${KERNEL}-${DEVICE}-${KERNELTYPE}-$(TZ=Asia/Jakarta date +%y%m%d-%H%M)"
-TEMPZIPNAME="${KERNELNAME}.zip"
-ZIPNAME="${KERNELNAME}.zip"
+#apt update -y && apt upgrade -y && apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && apt install build-essential -y && apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && apt install pigz -y && apt install python2 -y && apt install python3 -y
 
 
-# Telegram
-CHATIDQ="-1002001516627"
-CHATID="-1002001516627"
-TELEGRAM_TOKEN="6661722181:AAHA86zdVQTkkAU7FQowuZa1RJk7tCBQCmY"
+# Scrip option
+while (( ${#} )); do
+    case ${1} in
+        "-Z"|"--zip") ZIP=true ;;
+    esac
+    shift
+done
+[[ -z ${ZIP} ]] && { echo "${bold}LOADING-_-....${normal}"; }
 
-# Export Telegram.sh
-TELEGRAM_FOLDER=/workspace/telegram
-if ! [ -d "${TELEGRAM_FOLDER}" ]; then
-    git clone https://github.com/nekoprjkt/telegram.sh/ "${TELEGRAM_FOLDER}"
+DEFCONFIG="vince_defconfig"
+export KBUILD_BUILD_USER=Rsyd58
+export TZ=Asia/Jakarta
+export KBUILD_BUILD_HOST=non-pangu-pod
+export VER="V1.0.24.6.$(date +%d).DEV"
+export KERNELDIR="/workspace/voting-app/krnlv"
+export USE_CCACHE=1
+export CCACHE_DIR="workspace/voting-app/.ccache"
+export KERNELNAME="Furina"
+export SRCDIR="${KERNELDIR}"
+export OUTDIR="${KERNELDIR}/out"
+export ANYKERNEL="${KERNELDIR}/AnyKernel3"
+export DEFCONFIG="vince_defconfig"
+export ZIP_DIR="${KERNELDIR}/files"
+export IMAGE="${OUTDIR}/arch/arm64/boot/Image.gz-dtb"
+export ZIPNAME="${KERNELNAME}-Kernel-HM5Plus-$(date +%m%d-%H%M%S).zip"
+export FINAL_ZIP="${ZIP_DIR}/${ZIPNAME}"
+TC_DIR="/workspace/voting-app/azure-clang"
+export PATH="$TC_DIR/bin:$PATH"
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
 fi
 
-TELEGRAM="${TELEGRAM_FOLDER}"/telegram
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+rm -rf out
+fi
 
-tg_cast() {
-    "${TELEGRAM}" -t "${TELEGRAM_TOKEN}" -c "${CHATID}" -H \
-    "$(
-        for POST in "${@}"; do
-            echo "${POST}"
-        done
-    )"
-}
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+export LOCALVERSION="-フリーナ-${VER}-LTS"
+export KSU=y
+make -j$(nproc --all) O=out ARCH=arm64  CC=clang HOSTCC=clang HOSTCXX=clang++ READELF=llvm-readelf HOSTAR=llvm-ar AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-  2>&1 | tee log.txt
+#make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
+    echo -e "==========================="
+    echo -e "   COMPILE KERNEL COMPLETE "
+    echo -e "==========================="
 
-# Regenerating Defconfig
-regenerate() {
-    cp out/.config arch/arm64/configs/"${DEFCONFIG}"
-    git add arch/arm64/configs/"${DEFCONFIG}"
-    git commit -m "defconfig: Regenerate"
-}
+# Make ZIP using AnyKernel
+# ================
+echo -e "Copying kernel image";
+rm -rf ${ZIP_DIR}/*
+cp -v "${IMAGE}" "${ANYKERNEL}/";
+cd -;
+cd ${ANYKERNEL};
+zip -r9 ${FINAL_ZIP} *;
+gh release upload upstream-4.14.y ${FINAL_ZIP} -R github.com/Rsyd58/android_kernel_xiaomi_vince;
+cd -;
 
-# Building
-makekernel() {
-    echo ".........................."
-    echo ".     Building Kernel    ."
-    echo ".........................."
-    export PATH="/workspace/clang/bin:$PATH"
-#    export CROSS_COMPILE=${KERNEL_DIR}/gcc/bin/aarch64-maestro-linux-gnu-
-#    export CROSS_COMPILE_ARM32=${KERNEL_DIR}/gcc32/bin/arm-maestro-linux-gnueabi-
-    rm -rf "${KERNEL_DIR}"/out/arch/arm64/boot # clean previous compilation
-    mkdir -p out
-    make O=out ARCH=arm64 ${DEFCONFIG}
-    if [[ "${REGENERATE_DEFCONFIG}" =~ "true" ]]; then
-        regenerate
-    fi
-    make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64
+#echo -e "zip boot and dtbo"
+#rm -rf kernel.zip && cd out/arch/arm64/boot && zip kernel.zip Image.gz-dtb && mv kernel.zip ../../../../
 
-# Check If compilation is success
-    if ! [ -f "${KERN_IMG}" ]; then
-        END=$(TZ=Asia/Jakarta date +"%s")
-        DIFF=$(( END - START ))
-        echo -e "Kernel compilation failed, See buildlog to fix errors"
-        tg_cast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check Instance for errors"
-        exit 1
-    fi
-}
-
-# Packing kranul
-packingkernel() {
-    echo "........................"
-    echo ".    Packing Kernel    ."
-    echo "........................"
-    # Copy compiled kernel
-    if [ -d "${ANYKERNEL}" ]; then
-        rm -rf "${ANYKERNEL}"
-    fi
-    git clone "$ANYKERNEL_REPO" -b "$ANYKERNEL_BRANCH" "${ANYKERNEL}"
-     #   mkdir "${ANYKERNEL}"/kernel/
-        cp "${KERN_IMG}" "${ANYKERNEL}"/Image.gz-dtb
-   #     mkdir "${ANYKERNEL}"/dtb-nontreble/
-  #      cp "${KERN_DTB_NONTB}" "${ANYKERNEL}"/dtb-nontreble/msm8953-qrd-sku3-tissot-nontreble.dtb
-  #  mkdir "${ANYKERNEL}"/dtb-treble/
-   #     cp "${KERN_DTB_NONTB}" "${ANYKERNEL}"/dtb-treble/msm8953-qrd-sku3-tissot-nontreble.dtb
-
-    # Zip the kernel, or fail
-    cd "${ANYKERNEL}" || exit
-    zip -r9 "${TEMPZIPNAME}" ./*
-
-    # Sign the zip before sending it to Telegram
-   # curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-   # java -jar zipsigner-3.0.jar "${TEMPZIPNAME}" "${ZIPNAME}"
-
-    # Ship it to the CI channel
-    "${TELEGRAM}" -f "$ZIPNAME" -t "${TELEGRAM_TOKEN}" -c "${CHATIDQ}" 
-}
-
-# Starting
-tg_cast "<b>STARTING KERNEL BUILD</b>" \
-    "Device: ${DEVICE}" \
-    "Kernel Name: ${KERNEL}" \
-    "Build Type: ${KERNELTYPE}"
-START=$(TZ=Asia/Jakarta date +"%s")
-makekernel
-packingkernel
-END=$(TZ=Asia/Jakarta date +"%s")
-DIFF=$(( END - START ))
-tg_cast "Build for ${DEVICE} with ${COMPILER_STRING} <b>succeed</b> took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! "
-
-    echo "........................"
-    echo ".    Build Finished    ."
-    echo "........................"
+if [[ ":v" ]]; then
+exit
+fi
+#gh release upload test files/${FINAL_ZIP} -R github.com/Rsyd58/Rsyd58
